007ac8366ce6666e49cb4c9188464805e36f5421
emeibech <98073805+emeibech@users.noreply.github.com>
2023-05-25 22:20:21 +0800

    Create fetchData function for easier fetching

---
+++ src/data/fetchData.js
@@ -0,0 +1,12 @@
+const fetchData = async (url) => {
+  try {
+    const res = await fetch(url);
+    const data = await res.json();
+    return data;
+  } catch (error) {
+    console.error(error);
+    return error;
+  }
+};
+
+export default fetchData;
