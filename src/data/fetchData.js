6feffda36aa20ec1533bfec702c46efa5e74bc9d
emeibech <98073805+emeibech@users.noreply.github.com>
2023-05-27 00:11:56 +0800

    Modify functions for better flow of data and error handling

--- src/data/fetchData.js
+++ src/data/fetchData.js
@@ -1,11 +1,13 @@
 const fetchData = async (url) => {
   try {
     const res = await fetch(url);
+    // Return status if response is not ok
+    if (!res.ok) return res.status;
+    // Parse response data
     const data = await res.json();
     return data;
   } catch (error) {
     console.error(error);
-    return null;
   }
 };
 
--- src/data/getCity.js
+++ src/data/getCity.js
@@ -3,10 +3,14 @@ import fetchData from './fetchData';
 const getCity = async () => {
   try {
     const data = await fetchData('https://emeibechserver.com/ipgeo');
+    // Return info about the error
+    if (data === undefined) return 'An error occurred in the server';
+    // Return data, which contains the status code
+    if (data > 399) return data;
+    // Return city if fetching succeeds
     return data.city;
   } catch (error) {
     console.error(error);
-    return null;
   }
 };
 
