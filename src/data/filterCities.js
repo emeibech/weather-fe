a04a9d23fb56abd8b6c460fc904353a57ef745b0
emeibech <98073805+emeibech@users.noreply.github.com>
2023-05-30 01:12:09 +0800

    Create filterCities fn for geo lookup

---
+++ src/data/filterCities.js
@@ -0,0 +1,12 @@
+import cities from './cities.json';
+
+const filterCities = (input) => cities.filter((info) => {
+  // Get trimmed version of city names based on the length of user input
+  const trimmedCityName = info.city.slice(0, input.length);
+  // Capitalize the first letter of input in case it's in lowercase
+  const capitalizedInput = input.charAt().toUpperCase() + input.slice(1);
+  // Return true if they matched
+  return trimmedCityName === capitalizedInput;
+});
+
+export default filterCities;
