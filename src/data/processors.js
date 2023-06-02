c77bd33e0d4576a8e2d450ae6b9cf8a5350be8c0
emeibech <98073805+emeibech@users.noreply.github.com>
2023-06-03 01:33:36 +0800

    Create processor functions

---
+++ src/data/processors.js
@@ -0,0 +1,87 @@
+const getPopPercentage = (num) => `${Math.round(num * 100)}%`;
+const toCelsius = (K) => `${Math.round(K - 273.15)}°C`;
+const toFahrenheit = (K) => `${Math.round(1.8 * (K - 273.15) + 32)}°F`;
+
+const capitalize = (str) => str
+  .split(' ')
+  .map((word) => word.charAt().toUpperCase() + word.slice(1))
+  .join(' ');
+
+const completeUVI = (uvi) => {
+  const roundedUvi = Math.round((uvi * 100) / 100);
+  if (roundedUvi < 3) return `${roundedUvi} Low`;
+  if (roundedUvi < 6 && roundedUvi > 2) return `${roundedUvi} Moderate`;
+  if (roundedUvi < 8 && roundedUvi > 5) return `${roundedUvi} High`;
+  if (roundedUvi < 11 && roundedUvi > 7) return `${roundedUvi} Very High`;
+  return `${roundedUvi} Extreme`;
+};
+
+const getDay = (unix) => {
+  const date = new Date(unix * 1000);
+  const day = date.getDay();
+  const days = [
+    'Sunday',
+    'Monday',
+    'Tuesday',
+    'Wednesday',
+    'Thursday',
+    'Friday',
+    'Saturday',
+  ];
+
+  return days[day];
+};
+
+const getReadableTimestamp = (unix) => {
+  // Convert unix to milliseconds and get current date
+  const date = new Date(unix * 1000);
+  // Get the hour (in 24-hour format) and minutes
+  let hours = date.getHours();
+  let minutes = date.getMinutes();
+  // Determine AM/PM
+  const ampm = hours >= 12 ? 'PM' : 'AM';
+  // Convert to 12-hour format
+  hours %= 12;
+  // Convert 0s to 12
+  hours = hours || 12;
+  // Add leading zeros if necessary
+  hours = hours < 10 ? `0${hours}` : hours;
+  minutes = minutes < 10 ? `0${minutes}` : minutes;
+  // Return the formatted time string
+  return `${hours}:${minutes} ${ampm}`;
+};
+
+const getWindDirection = (windDeg) => {
+  const directions = [
+    'N',
+    'NNE',
+    'NE',
+    'ENE',
+    'E',
+    'ESE',
+    'SE',
+    'SSE',
+    'S',
+    'SSW',
+    'SW',
+    'WSW',
+    'W',
+    'WNW',
+    'NW',
+    'NNW',
+  ];
+
+  const index = Math.round(windDeg / 22.5) % 16;
+  return directions[index];
+};
+
+export {
+  getPopPercentage,
+  toCelsius,
+  toFahrenheit,
+  capitalize,
+  completeUVI,
+  getDay,
+  getReadableTimestamp,
+  getWindDirection,
+};
