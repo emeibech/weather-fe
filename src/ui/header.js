637da7caa9863f70cd23f8999dbbde6c7eaa6dff
emeibech <98073805+emeibech@users.noreply.github.com>
2023-06-08 02:36:17 +0800

    Create simple header

---
+++ src/ui/header.js
@@ -0,0 +1,29 @@
+const header = () => {
+  const parent = document.querySelector('#app');
+  const headerContainer = document.createElement('header');
+  headerContainer.classList.add('my-4', 'cursor-pointer');
+
+  const div = document.createElement('div');
+  div.classList.add(
+    'px-1',
+    'rounded-full',
+    'bg-fuchsia-700',
+    'h-9',
+    'w-9',
+  );
+
+  const h1 = document.createElement('h1');
+  h1.textContent = 'W!';
+  h1.classList.add(
+    'text-4xl',
+    'font-black',
+    'text-white',
+    'leading-none',
+  );
+
+  div.appendChild(h1);
+  headerContainer.appendChild(div);
+  parent.appendChild(headerContainer);
+};
+
+export default header;
