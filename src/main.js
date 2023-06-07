2e0cbb0e0dbc98a6ebf1ad0a0def877dcb7877d4
emeibech <98073805+emeibech@users.noreply.github.com>
2023-06-08 02:33:31 +0800

    Set up main.js for basic rendering

--- src/main.js
+++ src/main.js
@@ -1,5 +1,12 @@
 import './style.css';
+import fetchClientCity from './data/fetchClientCity';
+import fetchWeatherOC from './data/fetchWeatherOC';
+import filterData from './data/filterData';
+import processData from './data/processData';
+import header from './ui/header';
+import CurrentWeather from './ui/CurrentWeather';
 
 document.addEventListener('DOMContentLoaded', () => {
-
+  header();
+  const current = CurrentWeather();
 });
