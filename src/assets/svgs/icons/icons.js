27c8a87f781a82fe9e04d65668de4ed6679cada7
emeibech <98073805+emeibech@users.noreply.github.com>
2023-06-09 15:25:08 +0800

    Add icon assets for weather description

---
+++ src/assets/svgs/icons/icons.js
@@ -0,0 +1,41 @@
+import clearSkyDay from './01d@4x.png';
+import clearSkyNight from './01n@4x.png';
+import fewCloudsDay from './02d@4x.png';
+import fewCloudsNight from './02n@4x.png';
+import scatteredCloudsDay from './03d@4x.png';
+import scatteredCloudsNight from './03n@4x.png';
+import brokenCloudsDay from './04d@4x.png';
+import brokenCloudsNight from './04n@4x.png';
+import showerRainDay from './09d@4x.png';
+import showerRainNight from './09n@4x.png';
+import rainDay from './10d@4x.png';
+import rainNight from './10n@4x.png';
+import thunderstormDay from './11d@4x.png';
+import thunderstormNight from './11n@4x.png';
+import snowDay from './13d@4x.png';
+import snowNight from './13n@4x.png';
+import mistDay from './50d@4x.png';
+import mistNight from './50n@4x.png';
+
+const icons = {
+  '01d': clearSkyDay,
+  '01n': clearSkyNight,
+  '02d': fewCloudsDay,
+  '02n': fewCloudsNight,
+  '03d': scatteredCloudsDay,
+  '03n': scatteredCloudsNight,
+  '04d': brokenCloudsDay,
+  '04n': brokenCloudsNight,
+  '09d': showerRainDay,
+  '09n': showerRainNight,
+  '10d': rainDay,
+  '10n': rainNight,
+  '11d': thunderstormDay,
+  '11n': thunderstormNight,
+  '13d': snowDay,
+  '13n': snowNight,
+  '50d': mistDay,
+  '50n': mistNight,
+};
+
+export default icons;
