7818fa1ee574ed37b95bd173224880d4ff6e7d64
emeibech <98073805+emeibech@users.noreply.github.com>
2023-06-11 10:19:30 +0800

    Replace div with section and add more days

--- src/ui/DailyForecast.js
+++ src/ui/DailyForecast.js
@@ -1,45 +1,121 @@
 import {
-  DivFactory,
+  // DivFactory,
   TextFactory,
-  ImgFactory,
-  MoreInfoFactory,
   DailySummary,
 } from './elementFactories';
-import icons from '../assets/icons/icons';
 
 const DailyForecast = ({ isLoading, parent }) => {
-  const dailyForecastDiv = DivFactory({
-    isLoading,
-    parent,
-    name: 'dailyForecastDiv',
-    style: 'flex flex-col place-items-center',
-  });
+  const dailyForecastSection = (() => {
+    const section = document.createElement('section');
+    section.setAttribute('data-name', 'dailyForecastSection');
+
+    if (!isLoading) {
+      section.className = 'flex flex-col place-items-center';
+    }
+
+    parent.appendChild(section);
+
+    const removeElement = () => parent.removeChild(section);
+
+    return {
+      section,
+      parent,
+      removeElement,
+    };
+  })();
 
   const daily = TextFactory({
     isLoading,
-    parent: dailyForecastDiv.div,
+    parent: dailyForecastSection.section,
     name: 'daily',
     text: 'Daily',
     type: 'h2',
-    style: 'text-2xl',
+    style: 'text-2xl py-4',
   });
 
-  const daySummary = DailySummary({
+  const today = DailySummary({
     isLoading,
-    parent: dailyForecastDiv.div,
+    parent: dailyForecastSection.section,
     day: 'Today',
     icon: '10d',
-    description: 'Heavy Intensity Rain',
-    tempDay: 'Day: 28°C',
-    tempNight: 'Night: 20°C',
-    feelsLikeDay: 'Feels like: 36°C',
-    feelsLikeNight: 'Feels like: 24°C',
+    tempDay: '31°C',
+    tempNight: '27°C',
+  });
+
+  const tomorrow = DailySummary({
+    isLoading,
+    parent: dailyForecastSection.section,
+    day: 'Tomorrow',
+    icon: '10d',
+    tempDay: '28°C',
+    tempNight: '26°C',
+  });
+
+  const monday = DailySummary({
+    isLoading,
+    parent: dailyForecastSection.section,
+    day: 'Monday',
+    icon: '11d',
+    tempDay: '28°C',
+    tempNight: '27°C',
+  });
+
+  const tuesday = DailySummary({
+    isLoading,
+    parent: dailyForecastSection.section,
+    day: 'Tuesday',
+    icon: '10d',
+    tempDay: '31°C',
+    tempNight: '27°C',
+  });
+
+  const wednesday = DailySummary({
+    isLoading,
+    parent: dailyForecastSection.section,
+    day: 'Wednesday',
+    icon: '10d',
+    tempDay: '31°C',
+    tempNight: '27°C',
+  });
+
+  const thursday = DailySummary({
+    isLoading,
+    parent: dailyForecastSection.section,
+    day: 'Thursday',
+    icon: '09d',
+    tempDay: '30°C',
+    tempNight: '27°C',
+  });
+
+  const friday = DailySummary({
+    isLoading,
+    parent: dailyForecastSection.section,
+    day: 'Friday',
+    icon: '02d',
+    tempDay: '31°C',
+    tempNight: '27°C',
+  });
+
+  const saturday = DailySummary({
+    isLoading,
+    parent: dailyForecastSection.section,
+    day: 'Saturday',
+    icon: '01d',
+    tempDay: '30°C',
+    tempNight: '27°C',
   });
 
   return {
-    dailyForecastDiv,
+    dailyForecastSection,
     daily,
-    daySummary,
+    today,
+    tomorrow,
+    monday,
+    tuesday,
+    wednesday,
+    thursday,
+    friday,
+    saturday,
   };
 };
 
