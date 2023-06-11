fc8a947aa4e65549d518f32d2ea080bebe178daa
emeibech <98073805+emeibech@users.noreply.github.com>
2023-06-11 10:21:01 +0800

    Create main container parent

--- src/ui/Location.js
+++ src/ui/Location.js
@@ -1,12 +1,33 @@
 import { TextFactory } from './elementFactories';
 
-const Location = ({ isLoading, parent }) => TextFactory({
-  isLoading,
-  parent,
-  name: 'location',
-  text: 'Tokyo, Japan',
-  type: 'h2',
-  style: 'text-lg text-left px-8',
-});
+const Location = ({ isLoading, app }) => {
+  const main = (() => {
+    const parent = document.createElement('main');
+    parent.setAttribute('data-name', 'main');
+
+    app.appendChild(parent);
+
+    const removeElement = () => parent.removeChild(parent);
+
+    return {
+      parent,
+      removeElement,
+    };
+  })();
+
+  const location = TextFactory({
+    isLoading,
+    parent: main.parent,
+    name: 'location',
+    text: 'Tokyo, Japan',
+    type: 'h2',
+    style: 'text-lg text-left px-8',
+  });
+
+  return {
+    main,
+    location,
+  };
+};
 
 export default Location;
