8243d53cb1150bc74e0fb15b49c9ad416008b395
emeibech <98073805+emeibech@users.noreply.github.com>
2023-06-14 16:00:32 +0800

    Refactor how daily summaries are instantiated

--- src/ui/DailyForecast.js
+++ src/ui/DailyForecast.js
@@ -1,7 +1,7 @@
 import { SectionFactory, TextFactory } from './elementFactories';
 import DailySummary from './DailySummary';
 
-const DailyForecast = ({ isLoading, parent }) => {
+const DailyForecast = ({ isLoading, parent, data }) => {
   const dailyForecastSection = SectionFactory({
     parent,
     isLoading,
@@ -9,98 +9,39 @@ const DailyForecast = ({ isLoading, parent }) => {
     style: 'flex flex-col place-items-center',
   });
 
-  const daily = TextFactory({
+  const dailyHeader = TextFactory({
     isLoading,
     parent: dailyForecastSection.section,
-    name: 'daily',
+    name: 'dailyHeader',
     text: 'Daily',
     type: 'h2',
     style: 'text-2xl py-4',
   });
 
-  const today = DailySummary({
-    isLoading,
-    parent: dailyForecastSection.section,
-    day: 'Today',
-    icon: '10d',
-    tempDay: '31°C',
-    tempNight: '27°C',
-  });
+  const daily = data.map((dailyData) => {
+    const summary = DailySummary({
+      isLoading,
+      parent: dailyForecastSection.section,
+      day: dailyData.dt,
+      icon: dailyData.icon,
+      tempDay: dailyData.tempDay,
+      tempNight: dailyData.tempNight,
+    });
 
-  const tomorrow = DailySummary({
-    isLoading,
-    parent: dailyForecastSection.section,
-    day: 'Tomorrow',
-    icon: '10d',
-    tempDay: '28°C',
-    tempNight: '26°C',
-  });
-
-  const monday = DailySummary({
-    isLoading,
-    parent: dailyForecastSection.section,
-    day: 'Monday',
-    icon: '11d',
-    tempDay: '28°C',
-    tempNight: '27°C',
-  });
-
-  const tuesday = DailySummary({
-    isLoading,
-    parent: dailyForecastSection.section,
-    day: 'Tuesday',
-    icon: '10d',
-    tempDay: '31°C',
-    tempNight: '27°C',
-  });
-
-  const wednesday = DailySummary({
-    isLoading,
-    parent: dailyForecastSection.section,
-    day: 'Wednesday',
-    icon: '10d',
-    tempDay: '31°C',
-    tempNight: '27°C',
-  });
-
-  const thursday = DailySummary({
-    isLoading,
-    parent: dailyForecastSection.section,
-    day: 'Thursday',
-    icon: '09d',
-    tempDay: '30°C',
-    tempNight: '27°C',
-  });
-
-  const friday = DailySummary({
-    isLoading,
-    parent: dailyForecastSection.section,
-    day: 'Friday',
-    icon: '02d',
-    tempDay: '31°C',
-    tempNight: '27°C',
-  });
-
-  const saturday = DailySummary({
-    isLoading,
-    parent: dailyForecastSection.section,
-    day: 'Saturday',
-    icon: '01d',
-    tempDay: '30°C',
-    tempNight: '27°C',
+    return summary;
   });
 
   return {
     dailyForecastSection,
-    daily,
-    today,
-    tomorrow,
-    monday,
-    tuesday,
-    wednesday,
-    thursday,
-    friday,
-    saturday,
+    dailyHeader,
+    today: daily[0],
+    tomorrow: daily[1],
+    monday: daily[2],
+    tuesday: daily[3],
+    wednesday: daily[4],
+    thursday: daily[5],
+    friday: daily[6],
+    saturday: daily[7],
   };
 };
 
