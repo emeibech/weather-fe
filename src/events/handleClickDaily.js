674a04fcfd753531c128b96c738afdc6af01c6d7
emeibech <98073805+emeibech@users.noreply.github.com>
2023-06-16 07:44:42 +0800

    Put collapse and expand inside handleClick

--- src/events/handleClickDaily.js
+++ src/events/handleClickDaily.js
@@ -1,11 +1,31 @@
-import expand from './expand';
+import DailyFullInfo from '../ui/DailyFullInfo';
 
-const handleClickDaily = (daily) => {
-  const dailyArr = Object.entries(daily).filter(
-    (obj) => obj[0] !== 'dailyForecastSection' && obj[0] !== 'dailyHeader',
-  );
-
-  expand(dailyArr);
+const collapse = (full) => {
+  full.fullInfoSection.section.addEventListener('click', () => {
+    full.summary.render(full.fullInfoSection.section.nextSibling);
+    full.removeFromDom();
+  });
 };
+
+const expand = ({ dailyArr, data }) => {
+  dailyArr.forEach((day, index) => {
+    day.dailyDiv.div.addEventListener('click', () => {
+      const fullInfo = DailyFullInfo({
+        parent: day.dailyDiv.div.parentElement,
+        name: data[index].dt,
+        summary: day,
+        nextElem: day.dailyDiv.div.nextSibling,
+        data: data[index],
+      });
+
+      day.removeFromDom();
+      fullInfo.render();
+
+      collapse(fullInfo);
+    });
+  });
+};
+
+const handleClickDaily = ({ dailyArr, data }) => expand({ dailyArr, data });
 
 export default handleClickDaily;
