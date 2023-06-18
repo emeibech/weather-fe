468d8ac30d2a6b0421e0edde23fa13bfbebc9f05
emeibech <98073805+emeibech@users.noreply.github.com>
2023-06-18 19:57:52 +0800

    Create unit toggler button

---
+++ src/ui/UnitTogglerBtn.js
@@ -0,0 +1,28 @@
+const UnitTogglerBtn = ({ isFahrenheit, parent }) => {
+  const button = document.createElement('button');
+  button.textContent = (isFahrenheit) ? '°C' : '°F';
+  button.className = 'text-xl rounded-full shadow-lg text-zinc-200';
+  button.setAttribute('data-name', 'unittoggler');
+  button.setAttribute(
+    'data-activeunit',
+    (isFahrenheit) ? 'imperial' : 'metric',
+  );
+
+  parent.appendChild(button);
+
+  const removeElement = () => parent.removeChild(button);
+  const render = () => parent.appendChild(button);
+  const setText = (str) => {
+    button.textContent = str;
+  };
+
+  return {
+    isFahrenheit,
+    button,
+    removeElement,
+    render,
+    setText,
+  };
+};
+
+export default UnitTogglerBtn;
