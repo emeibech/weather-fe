9fb5ca7e4b7531ae76fdc210b438f205bee9dc50
emeibech <98073805+emeibech@users.noreply.github.com>
2023-06-20 22:17:48 +0800

    Fix bug on daily summary expand function

--- src/ui/FullInfoArray.js
+++ src/ui/FullInfoArray.js
@@ -2,13 +2,18 @@ import DailyFullInfo from './DailyFullInfo';
 
 const FullInfoArray = ({ dailyArr, metric, imperial }) => {
   const fullInfoArr = dailyArr.map((day, index) => {
+    const prev = dailyArr[index - 1];
+
     const fullInfo = DailyFullInfo({
       parent: day.dailyDiv.div.parentElement,
       name: metric[index].dt,
       summary: day,
-      nextElem: day.dailyDiv.div.nextSibling,
       metric: metric[index],
       imperial: imperial[index],
+      nextElem: (() => {
+        if (index !== 0) return prev.dailyDiv.div.nextSibling;
+        return day.dailyDiv.div.parentElement.firstChild.nextSibling;
+      })(),
     });
 
     return fullInfo;
