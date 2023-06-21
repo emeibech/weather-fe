f76578921e1631d0d5e7967b87abcb8f59e4c78f
emeibech <98073805+emeibech@users.noreply.github.com>
2023-06-21 11:29:25 +0800

    Create UI for search bar

---
+++ src/ui/SearchBar.js
@@ -0,0 +1,71 @@
+import { ImgFactory } from './elementFactories';
+import search from '../assets/svgs/search-gray.svg';
+import clear from '../assets/svgs/clear-white.svg';
+
+const SearchBar = (parent) => {
+  const containerForm = (() => {
+    const form = document.createElement('form');
+    form.setAttribute('data-name', 'containerForm');
+    form.className = 'grid items-center';
+
+    parent.appendChild(form);
+
+    const removeFromDom = () => parent.removeChild(form);
+    const render = (next) => parent.insertBefore(form, next);
+
+    return {
+      form,
+      removeFromDom,
+      render,
+    };
+  })();
+
+  const searchInput = (() => {
+    const input = document.createElement('input');
+    input.setAttribute('type', 'search');
+    input.setAttribute('id', 'search');
+    input.setAttribute('name', 'search');
+    input.setAttribute('placeholder', 'Search');
+    input.setAttribute('autocomplete', 'off');
+    input.setAttribute('data-name', 'searchInput');
+
+    containerForm.form.appendChild(input);
+
+    return input;
+  })();
+
+  const searchSvg = ImgFactory({
+    parent: containerForm.form,
+    name: 'searchSvg',
+    src: search,
+    style: 'h-4 w-4',
+  });
+
+  const clearBtn = (() => {
+    const button = document.createElement('button');
+    button.setAttribute('data-name', 'clearBtn');
+    button.setAttribute('type', 'button');
+    button.className = 'hidden';
+
+    containerForm.form.appendChild(button);
+
+    return button;
+  })();
+
+  const clearSvg = ImgFactory({
+    parent: clearBtn,
+    name: 'clearSvg',
+    src: clear,
+    style: 'h-4 w-4',
+  });
+
+  return {
+    containerForm,
+    searchInput,
+    clearBtn,
+    searchSvg,
+    clearSvg,
+  };
+};
+
+export default SearchBar;
