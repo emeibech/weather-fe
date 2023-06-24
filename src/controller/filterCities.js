01a93a41e076e8ac64292f77882e5bef09de9aa9
emeibech <98073805+emeibech@users.noreply.github.com>
2023-06-25 04:44:22 +0800

    Capitalize all word and not just first letter

--- src/controller/filterCities.js
+++ src/controller/filterCities.js
@@ -1,10 +1,11 @@
 import cities from '../data/cities.json';
+import { capitalize } from '../data/processors';
 
 const filterCities = (input) => cities.filter((info) => {
   // Get trimmed version of city names based on the length of user input
   const trimmedCityName = info.city.slice(0, input.length);
-  // Capitalize the first letter of input in case it's in lowercase
-  const capitalizedInput = input.charAt().toUpperCase() + input.slice(1);
+  // Capitalize the first letter of each word in case it's lowercased
+  const capitalizedInput = capitalize(input);
   // Return true if they matched
   return trimmedCityName === capitalizedInput;
 });
