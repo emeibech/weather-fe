406c05a47a89d8d1a89712fd3bffa4c3b8dc9536
emeibech <98073805+emeibech@users.noreply.github.com>
2023-06-24 22:15:13 +0800

    Empty list when focusing on search without value

--- src/events/handleFocusSearch.js
+++ src/events/handleFocusSearch.js
@@ -1,5 +1,9 @@
 const handleFocusSearch = ({ search, dropdown }) => {
   search.searchInput.addEventListener('focus', () => {
+    if (search.searchInput.value === '') {
+      dropdown.emptyList();
+    }
+
     dropdown.showDropdown();
   });
 };
