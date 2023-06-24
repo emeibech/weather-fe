39d575cca3a76176c322c55b6eceaa98c6c3c7c4
emeibech <98073805+emeibech@users.noreply.github.com>
2023-06-24 22:18:27 +0800

    Trigger city search when a suggestion is clicked

--- src/events/handleBlurSearch.js
+++ src/events/handleBlurSearch.js
@@ -1,7 +1,43 @@
+import handleClickDropdown from './handleClickDropdown';
+import getCountryCode from '../controller/getCountryCode';
+
 const handleBlurSearch = ({ search, dropdown }) => {
   const input = search.searchInput;
 
-  input.addEventListener('blur', () => {
+  input.addEventListener('blur', (e) => {
+    const target = e.explicitOriginalTarget;
+
+    if (target.nodeName === 'LI') {
+      [input.value] = target.textContent.split(',');
+
+      handleClickDropdown({
+        lat: target.getAttribute('data-lat'),
+        lon: target.getAttribute('data-lon'),
+        city: target.textContent.split(',')[0],
+        country: target.textContent.split(', ')[1],
+        countryCode: getCountryCode(target.textContent.split(', ')[1]),
+      });
+
+      input.value = '';
+    }
+
+    if (target.nodeName === '#text') {
+      if (target.parentElement.nodeName === 'LI') {
+        const li = target.parentElement;
+        [input.value] = li.textContent.split(',');
+
+        handleClickDropdown({
+          lat: li.getAttribute('data-lat'),
+          lon: li.getAttribute('data-lon'),
+          city: li.textContent.split(',')[0],
+          country: li.textContent.split(', ')[1],
+          countryCode: getCountryCode(li.textContent.split(', ')[1]),
+        });
+
+        input.value = '';
+      }
+    }
+
     dropdown.hideDropdown();
   });
 };
