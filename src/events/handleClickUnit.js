52be0aadd284740691f97bdf7d9f1526258983be
emeibech <98073805+emeibech@users.noreply.github.com>
2023-06-24 22:16:28 +0800

    Refactor toggler logic

--- src/events/handleClickUnit.js
+++ src/events/handleClickUnit.js
@@ -1,22 +1,26 @@
-const handleClickUnit = ({ toggler, variableUnits }) => {
-  toggler.button.addEventListener('click', () => {
-    const unit = toggler.button.getAttribute('data-activeunit');
-    toggler.setText((unit === 'imperial') ? '°F' : '°C');
+const handleClickUnit = (variableUnits) => {
+  const toggler = document.querySelector('[data-name="unittoggler"]');
+
+  const toggle = () => {
+    const unit = toggler.getAttribute('data-activeunit');
+    toggler.textContent = (unit === 'imperial') ? '°F' : '°C';
 
     if (unit === 'metric') {
-      toggler.button.removeAttribute('data-activeunit');
-      toggler.button.setAttribute('data-activeunit', 'imperial');
+      toggler.removeAttribute('data-activeunit');
+      toggler.setAttribute('data-activeunit', 'imperial');
       variableUnits.forEach((element) => {
         element.changeToImperial();
       });
     } else {
-      toggler.button.removeAttribute('data-activeunit');
-      toggler.button.setAttribute('data-activeunit', 'metric');
+      toggler.removeAttribute('data-activeunit');
+      toggler.setAttribute('data-activeunit', 'metric');
       variableUnits.forEach((element) => {
         element.changeToMetric();
       });
     }
-  });
+  };
+
+  toggler.addEventListener('click', toggle);
 };
 
 export default handleClickUnit;
