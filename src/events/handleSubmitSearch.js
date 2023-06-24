7f75d03e80509e7358d5dceee66c88c79626a209
emeibech <98073805+emeibech@users.noreply.github.com>
2023-06-25 05:33:32 +0800

    Hide clear button on submit

--- src/events/handleSubmitSearch.js
+++ src/events/handleSubmitSearch.js
@@ -3,12 +3,14 @@ import weatherAlt from '../controller/weatherAlt';
 const handleSubmitSearch = (search) => {
   const { form } = search.containerForm;
   const input = search.searchInput;
+  const button = search.clearBtn;
   form.addEventListener('submit', (e) => {
     e.preventDefault();
     const { value } = search.searchInput;
     weatherAlt(value);
     search.searchInput.blur();
     input.value = '';
+    button.classList.add('hidden');
   });
 };
 
