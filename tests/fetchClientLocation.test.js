0d8dc83e7f2b185a4c102127e9ce00d6f9ab81f4
emeibech <98073805+emeibech@users.noreply.github.com>
2023-06-25 03:00:14 +0800

    Update test for fetching client location

--- tests/fetchClientLocation.test.js
+++ tests/fetchClientLocation.test.js
@@ -1,28 +1,28 @@
 // eslint-disable-next-line object-curly-newline
 import { describe, it, expect, beforeEach, vitest } from 'vitest';
-import fetchClientCity from '../src/data/fetchClientCity';
+import fetchClientLocation from '../src/data/fetchClientLocation';
 import fetchData from '../src/data/fetchData';
 
 // Mock the fetchData function for testing purposes
 vitest.mock('../src/data/fetchData');
 
-describe('fetchClientCity unit test', () => {
+describe('fetchClientLocation unit test', () => {
   beforeEach(() => vitest.clearAllMocks());
 
   it('returns client city if fetching succeeds', async () => {
     // Mock successful response
     const mockResponse = { city: 'New York' };
     fetchData.mockResolvedValue(mockResponse);
-    // Call the fetchClientCity function
-    const city = await fetchClientCity();
+    // Call the fetchClientLocation function
+    const location = await fetchClientLocation();
     // Verify the response
-    expect(city).toEqual(mockResponse.city);
+    expect(location).toEqual(mockResponse);
   });
 
   it('returns an object containing the error on failed fetch', async () => {
     const mockResponse = { error: '500 Internal Server Error' };
     fetchData.mockResolvedValue(mockResponse);
-    const statusCode = await fetchClientCity();
+    const statusCode = await fetchClientLocation();
     expect(statusCode).toEqual(mockResponse);
   });
 });
