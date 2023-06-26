286237377ec70a279c17bbbb505ac9b340e85b59
emeibech <98073805+emeibech@users.noreply.github.com>
2023-06-26 14:52:00 +0800

    Prevent fetching data on empty search field

--- src/events/handleSubmitSearch.js
+++ src/events/handleSubmitSearch.js
@@ -4,9 +4,12 @@ const handleSubmitSearch = (search) => {
   const { form } = search.containerForm;
   const input = search.searchInput;
   const button = search.clearBtn;
-  form.addEventListener('submit', (e) => {
-    e.preventDefault();
+  form.addEventListener('submit', (event) => {
+    event.preventDefault();
     const { value } = search.searchInput;
+
+    if (value === '') return;
+
     weatherAlt(value);
     search.searchInput.blur();
     input.value = '';
